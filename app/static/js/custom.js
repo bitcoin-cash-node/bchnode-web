c5dd46c925762c7af2346e4682d9cc56cc5e3d40
Corentin Mercier <corentin@mercier.link>
2020-05-10 20:50:04 +0700

    Add chevron to language selector (+ rotate)

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -5,3 +5,12 @@ $(".navbar-toggler").click(function() {
   $(".navbar-toggler-icon")
     .toggleClass('navbar-expanded');
 });
+
+// Rotate chevron down on click
+$("#lang-selector").focus(function() {
+  $("#chevron-down").addClass('rotate');
+});
+
+$("#lang-selector").blur(function() {
+  $("#chevron-down").removeClass('rotate');
+});
