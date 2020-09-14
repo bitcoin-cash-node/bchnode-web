5683cbcf4ff6d25b701b18ab8ac27ee46c256412
aninit <leea@torreycommerce.com>
2020-09-14 16:12:45 +0700

    Add Newsroom multiple filter tags at once
    Add Newsroom multiple filter tags at once
    Add error messagingw when zero results

--- js/scripts.js
+++ js/scripts.js
@@ -21,12 +21,43 @@ chevrons.forEach(function(chevron, index) {
 
 // newsroom tags
 $('.btn-tag-toggle').click(function() {
-  $(this).addClass('active');
-  $(this).siblings().removeClass('active');
+  if ($(this).hasClass('active') && !$(this).hasClass('btn-tag-toggle-all')) {
+    $(this).removeClass('active');
+  } else {
+    $(this).addClass('active');
+  }
   if ($(this).hasClass('btn-tag-toggle-all')) {
+    $(this).siblings().removeClass('active');
+  } else {
+    if (!$('.btn-tag-toggle.active').length) {
+      $('.btn-tag-toggle-all').addClass('active');
+    } else {
+      $('.btn-tag-toggle-all').removeClass('active');
+    }
+  }
+  var showError = 1;
+  if ($('.btn-tag-toggle-all').hasClass('active')) {
+    showError = 0;
     $('.collapse-tag').collapse('show');
   } else {
-    $('.collapse-tag:not(.'+$(this).data('tag-target')+')').collapse('hide');
-    $('.'+$(this).data('tag-target')).collapse('show');
+    var tagActive = "";
+    $('[data-tag-target]').each(function() {
+      if ($(this).hasClass('active')) {
+        tagActive = tagActive.concat('.'+$(this).data('tag-target'));
+      }
+    });
+    $('.collapse-tag').each(function() {
+      if ($(this).is(tagActive)) {
+        showError = 0;
+        $(this).collapse('show');
+      } else {
+        $(this).collapse('hide');
+      }
+    });
+  }
+  if (showError) {
+    $('.collapse-tag-error').collapse('show');
+  } else {
+    $('.collapse-tag-error').collapse('hide');
   }
 });
